318f374d6fd18c0c7dc9fb7a2fb859ddc33e9602
BAsith <basith6k@gmail.com>
2024-12-06 20:49:59 +0530

    Last Commit

--- futuhtravels/settings.py
+++ futuhtravels/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-bw$$x%bgb#y+l(b@$3x&9&rnalm4xk!@20!o@!%v%rfinnbc46'
 
 # Initialize environment variables
 
