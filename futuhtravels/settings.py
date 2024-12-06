e002929134493c59666463f9415d4801ddf5ee4d
BAsith <basith6k@gmail.com>
2024-12-06 20:59:18 +0530

    Final Point

--- futuhtravels/settings.py
+++ futuhtravels/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bw$$x%bgb#y+l(b@$3x&9&rnalm4xk!@20!o@!%v%rfinnbc46'
+SECRET_KEY = config('SECRET_KEY')
 
 # Initialize environment variables
 
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-bw$$x%bgb#y+l(b@$3x&9&rnalm4xk!@20!o@!%v%rfinnbc46
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = True
-ALLOWED_HOSTS = ['127.0.0.1','.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 
